afab53b27da7a09c7de6d9a9b47aae5cc0b5086d
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-01-27 14:30:30 +1100

    update the hyper parameters for random forest

--- function_for_trainings.py
+++ function_for_trainings.py
@@ -38,7 +38,7 @@ def regression_training(X_train_scaled, X_test_scaled, y_train, y_test, plot=Fal
     model_names = ['KNN', 'Ridge Linear Regression', 'Random Forest', 'Neural Network', 'Gradient Boosting', 'Ada Boosting', 'Support Vector'] # a list of name for each model.
     model_lists = [KNeighborsRegressor(), Ridge(), RandomForestRegressor(), MLPRegressor(), GradientBoostingRegressor(), AdaBoostRegressor(), SVR()]# a list of model improted from sklearn
     gridsearchlist = [True, True, False, True, False, False, True]
-    param_list  = [{'n_neighbors':range(1, 30)}, {'alpha': [0.01, 0.1, 1, 10]}, {'n_estimators': [10, 100]}, {'hidden_layer_sizes':((100, 300, 300, 100), (100, 300, 500, 300, 100), (200, 600, 600, 200), (200, 600, 900, 600, 200))}, {'n_estimators':[10, 100]}, {'n_estimators':[10, 100]}, {'C': [0.1, 1, 10], 'epsilon': [1e-2, 0.1, 1]}]# a list of key parameters correspond to the models in the model_lists
+    param_list  = [{'n_neighbors':range(1, 30)}, {'alpha': [0.01, 0.1, 1, 10]}, {'n_estimators': [200, 100]}, {'hidden_layer_sizes':((100, 300, 300, 100), (100, 300, 500, 300, 100), (200, 600, 600, 200), (200, 600, 900, 600, 200))}, {'n_estimators':[10, 100]}, {'n_estimators':[10, 100]}, {'C': [0.1, 1, 10], 'epsilon': [1e-2, 0.1, 1]}]# a list of key parameters correspond to the models in the model_lists
 
     # prepare an emtply list to collect the predicted y
     y_pred_list = []
