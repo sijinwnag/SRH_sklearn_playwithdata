15ab2174b59d6bb5585bff12424f8ce6d8040594
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-01-30 20:53:09 +1100

    Update test_object.py

--- test_object.py
+++ test_object.py
@@ -4,8 +4,8 @@ sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata')
 # sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata')
 from MLobject import *
 
-df1 = MyMLdata(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect\lifetime_dataset_example.csv', 'Et_eV', 2)
+df1 = MyMLdata(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect\lifetime_dataset_example.csv', 'bandgap', 2)
 # df1.regression_repeat(n_repeat=1) # check if k regression works after converting into class.
-df1.perfor_singletask_ML(plot_graphs=True)
+df1.perform_alltasks_ML()
 
 list(range(7))
