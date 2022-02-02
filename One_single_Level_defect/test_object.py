e58703fad6ef096de537aaee8c7ccd5893b1de04
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-02-03 10:18:09 +1100

    dsa

--- One_single_Level_defect/test_object.py
+++ One_single_Level_defect/test_object.py
@@ -1,7 +1,7 @@
 import sys
 # import the function file from another folder:
-sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect')
-# sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata')
+# sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect')
+sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect')
 from MLobject import *
 
 df1 = MyMLdata(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect\2022-02-01-12-07-56_Main_datasetID_0.csv', 'Et_eV', 5)
@@ -14,7 +14,7 @@ scores[2]
 # scores[0].to_csv('k_regression.csv')
 # scores[1].to_csv('Et_regression.csv')
 # scores[2].to_csv('bandgap_classification.csv')
-df1.singletask = 'Et_minus'
+df1.singletask = 'Et_plus'
 scores = df1.regression_repeat()
 scores
 scores.to_csv('Et_minus_regression.csv')
