d3942fa7864031b5f302814b6f333806120ff79a
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-02-07 08:57:16 +1100

    works for data label

--- One_single_Level_defect/test_object.py
+++ One_single_Level_defect/test_object.py
@@ -4,13 +4,9 @@ import sys
 sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect')
 from MLobject import *
 
-df1 = MyMLdata(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect\lifetime_dataset_example.csv', 'Et_eV', 1)
+df1 = MyMLdata(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect\lifetime_dataset_example.csv', 'Et_eV', 5)
 # df1.regression_repeat(n_repeat=1) # check if k regression works after converting into class.
 scores = df1.perform_alltasks_ML()
-scores
-scores[0]
-scores[1]
-scores[2]
 # scores[0].to_csv('k_regression.csv')
 # scores[1].to_csv('Et_regression.csv')
 # scores[2].to_csv('bandgap_classification.csv')
@@ -18,3 +14,12 @@ df1.singletask = 'Et_plus'
 scores = df1.regression_repeat()
 # scores
 # scores.to_csv('Et_minus_regression.csv')
+# %%--- Testing lines
+import pandas as pd
+import matplotlib.pyplot as plt
+dataframe = pd.DataFrame([[1, 2], [3, 4]], columns=['1', '2'])
+
+plt.figure()
+plt.boxplot(dataframe, vert=False, labels=['s', 'd'])
+plt.show()
+# %%-
