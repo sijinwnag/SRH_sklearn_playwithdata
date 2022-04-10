6f15a73d63dd9a99225d14cbd5119706e2b346c4
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-04-10 16:16:37 +1000

    Update set11.py

--- 2_levels_problem/mode2/Et_regression/set11/set11.py
+++ 2_levels_problem/mode2/Et_regression/set11/set11.py
@@ -4,7 +4,7 @@ import sys
 # sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect')
 sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem')
 from MLobject_tlevel import *
-df1 = MyMLdata_2level(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\set11_1e15.csv', 'bandgap1', 1)
+df1 = MyMLdata_2level(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Et_regression\set11\set11_1e15.csv', 'bandgap1', 1)
 # np.shape(df1.data)
 # %%-
 
@@ -20,6 +20,6 @@ df1.singletask = 'Et_eV_2_known_Et_eV_2_plus'
 r2scores = df1.regression_repeat() # this makes the results better but has data leakage, test if adding the esmiated bandgap 1 will help: R2 got about 0.999.
 # %%-
 
-# %%-- Perform chain regression: Et1->Et1+Et2->Et2.
-chain_scores = df1.repeat_chain_regressor(repeat_num=5, regression_order=None)
-pd.DataFrame(np.array(chain_scores).reshape(35, 3)).to_csv('chainscores2')
+# %%-- Perform chain regression: Et1->Et1+Et2->Et or Et1->Et2
+chain_scores = df1.repeat_chain_regressor(repeat_num=5, regression_order=None, chain_name = 'Et1->Et2')
+# pd.DataFrame(np.array(chain_scores).reshape(35, 2)).to_csv(path_or_buf = r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Et_regression\set11\chainscore_two_steps.csv')
