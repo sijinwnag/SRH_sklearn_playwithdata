f2c1d14a22c24fc9ce56d7aea9abbc8792fc9f34
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-04-19 08:36:53 +1000

    Update set11.py

--- 2_levels_problem/mode2/Et_regression/set11/set11.py
+++ 2_levels_problem/mode2/Et_regression/set11/set11.py
@@ -4,14 +4,17 @@ import sys
 # sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\One_single_Level_defect')
 sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
 from MLobject_tlevel import *
-df1 = MyMLdata_2level(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Et_regression\set11\set11_1e15.csv', 'bandgap1', 5)
+# one doping level:
+# df1 = MyMLdata_2level(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Et_regression\set11\set11_1e15.csv', 'bandgap1', 5)
 # np.shape(df1.data)
+# multiple doping level:
+df1 = MyMLdata_2level(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Et_regression\set11\set11_diff_doping.csv', 'bandgap1', 5)
 # %%-
 
 # %%-- Single tasks.
 # %%-- Perform regression for Et single task.
 df1.singletask = 'Et_eV_1'
-r2scores = df1.regression_repeat() # R2 above 0.9
+r2scores = df1.regression_repeat()
 df1.singletask = 'Et_eV_2'
 r2scores = df1.regression_repeat() # R2 about 0.2 to 0.4
 df1.singletask = 'Et_eV_1+Et_eV_2'
