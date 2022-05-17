530e3f9fed43d48a5096baf757277ed9657dcaef
sijinwnag <63268286+sijinwnag@users.noreply.github.com>
2022-05-17 21:27:38 +1000

    add path way to workstation

--- 2_levels_problem/mode2/Et_regression/set11/set11.py
+++ 2_levels_problem/mode2/Et_regression/set11/set11.py
@@ -3,10 +3,11 @@ import sys
 
 # import the function file from another folder:
 # use this line if on hp laptop:
-sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
+# sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
 # use this line if on dell laptop
 # sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
 # use this line if on workstation
+sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
 from MLobject_tlevel import *
 
 # use this secion if using dell laptop
@@ -29,8 +30,11 @@ from MLobject_tlevel import *
 
 # use this section if using hp laptop:
 # one doping level:
-df1 = MyMLdata_2level(r'C:\ML_databank\set11_1e15.csv', 'bandgap1', 10)
+# df1 = MyMLdata_2level(r'C:\ML_databank\set11_1e15.csv', 'bandgap1', 10)
 # df1 = MyMLdata_2level(r'"C:\ML_databank\set11_1e15.csv"', 'bandgap1', 10)
+
+# use this section if using workstation
+# df1 = MyMLdata_2level(r'', 'bandgap1', 10)
 # %%-
 
 # %%-- different data engineering
