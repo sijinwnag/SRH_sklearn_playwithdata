6fbd113fb553ea03eb96105d7b2de3ef8f1a69a8
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-06-02 18:26:31 +1000

    Update set10.py

--- 2_levels_problem/mode2/Et_regression/set10/set10.py
+++ 2_levels_problem/mode2/Et_regression/set10/set10.py
@@ -9,7 +9,7 @@ sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\
 sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
 from MLobject_tlevel import *
 
-df1 = MyMLdata_2level(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\yoann_code_new\Savedir_example\outputs\2022-06-02-17-12-15_advanced example - multi_level_L_datasetID_0.csv', 'bandgap1',5)
+df1 = MyMLdata_2level(r'C:\Users\sijin wang\Documents\GitHub\yoann_code_new\Savedir_example\outputs\2022-06-02-18-12-52_advanced example - multi_level_L_datasetID_0.csv', 'bandgap1',5)
 # %%-
 
 # %%-- different data engineering before training ML model.
