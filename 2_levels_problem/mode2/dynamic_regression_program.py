a82b414ba09363d5f785a84289d026dc9c2bb616
sijinwnag <63268286+sijinwnag@users.noreply.github.com>
2022-07-05 08:51:49 +1000

    Update dynamic_regression_program.py

--- 2_levels_problem/mode2/dynamic_regression_program.py
+++ 2_levels_problem/mode2/dynamic_regression_program.py
@@ -71,7 +71,7 @@ def transparency_calculator(datasize):
     if datasize>800:
         alpha = 800/datasize*0.5
     else:
-        alpha = 0.5
+        alpha = 1
     return alpha
 # %%-
 
@@ -92,9 +92,9 @@ Plan:
 
 # load the data:
 # the dataset0 (the real validation set)
-dataset0 = pd.read_csv(r"G:\study\thesis_data_storage\set11\set11_8k.csv")
+# dataset0 = pd.read_csv(r"G:\study\thesis_data_storage\set11\set11_1.csv")
 # the dtaset1 (the dataset that varies everytyhing to train Et1, Sn1, Sp1)
-dataset1 = pd.read_csv(r"G:\study\thesis_data_storage\set11\set11_80000.csv")
+dataset1 = pd.read_csv(r"G:\study\thesis_data_storage\set11\set11_8k.csv")
 
 # select the lifeitme columes, the criteria is whether the colume title start with a number:
 select_X_list = []
@@ -167,7 +167,7 @@ plan:
 
 # load the validationset
 # the dataset0 (the real validation set)
-dataset0 = pd.read_csv(r"C:\Users\sijin wang\Desktop\Thesis\thesiswork\simulation_data\set11\set11_1.csv")
+dataset0 = pd.read_csv(r"G:\study\thesis_data_storage\set11\set11_10.csv")
 
 # select the lifetime data of hte validationste by choosing hte colume names start with a number.
 # select the lifeitme columes, the criteria is whether the colume title start with a number:
@@ -181,6 +181,7 @@ X_dataset0 = dataset0[select_X_list]
 X_log_dataset0 = np.log10(X_dataset0)
 
 y_list = []
+tasknames = ['Et_eV_1', 'logSn_1', 'logSp_1']
 # iterate through each task:
 for k in range(len(model_step1)):
     # select the correct machine learning model.
@@ -194,9 +195,123 @@ for k in range(len(model_step1)):
     # collect the prediction into hte list: it should be [Et1_predicted, logSn1_predicted, logSp1_predicted]
     y_list.append(y_pred_set0)
 
+    # collect the actual y data:
+    y_real = dataset0[tasknames[k]]
+
+    # calcualte hte R2 and mae.
+    r2 = r2_score(y_real, y_pred_set0)
+    mae = mean_absolute_error(y_real, y_pred_set0)
+
+    # plot the real vs prediction for each parameters:
+    plt.figure()
+    # calculate the transparency:
+    alpha=transparency_calculator(len(y_real))
+    print('transparency of scattering plot is ' + str(alpha))
+    plt.scatter(y_real, y_pred_set0, label=('$R^2$' + '=' + str(round(r2, 3))) + ('  Mean Absolue error' + '=' + str(round(mae, 3))), alpha=alpha)
+    plt.xlabel('real value')
+    plt.ylabel('predicted value')
+    plt.title('real vs predicted at trial ' + ' for task ' + str(taskname))
+    plt.legend()
+    plt.show()
+
 print(y_list)
 # %%-
 
+# %%-- for each point, given the y prediction, dynamically generate new data.
+# read off the second line from dataset0
+data1 = dataset0.iloc[1,:]
+Et1_real = data1['Et_eV_1']
+logSn1_real = data1['logSn_1']
+logSp1_real = data1['logSp_1']
+# suppose we have predicted Et1, logSn1, logSn2:
+Et1_real
+Et1_pred = 0.37315143
+logSn1_real
+logSn1_pred = -16.47345516
+logSp1_real
+logSp1_pred = -15.32641967
+
+# %%-- Inputs
+SAVEDIR = r"C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\yoann_code_new\Savedir_example" # you can change this to your own path
+# SAVEDIR = r"C:\Users\sijin wang\Documents\GitHub\yoann_code_new\Savedir_example"
+FILEPATH = "advanced_example\\data\\sample_original_L.csv"
+TEMPERATURE = [150, 200, 250, 300, 350, 400] # below 400K
+# DOPING = [1e14, 5e14, 1e15, 5e15, 1e16] # *len(TEMPERATURE) # make sure T and doping have same length
+# DOPING = [1e15]
+DOPING = [1e15] *len(TEMPERATURE) # make sure T and doping have same length
+# if using different doping levels: each temperature will match each doping.
+# so each element of temperature will repeat for length of itself times
+# TEMPERATURE
+WAFERTYPE = 'p'
+NAME = 'advanced example - multi_level_L'
+# %%-
+
+# %%-- Run this section only if you need multiple doping levels
+New_T = []
+for temp in TEMPERATURE:
+    for k in range(len(DOPING)):
+        New_T.append(temp)
+# repeat the whole doping 6 times.
+DOPING=DOPING*len(DOPING)
+# len(DOPING)
+TEMPERATURE=New_T
+# print(New_T)
+# %%-
+
+# %%-- Hyper-parameters
+PARAMETERS = {
+    'name': NAME,
+    'save': False,   # True to save a copy of the printed log, the outputed model and data
+    'logML':False,   #   Log the output of the console to a text file
+    'n_defects': 8000, # Size of simulated defect data set for machine learning
+    'dn_range' : np.logspace(13,17,100),# Number of points to interpolate the curves on
+    'non-feature_col':['Mode','Label',"Name","Et_eV_1","Sn_cm2_1","Sp_cm2_1",'k_1','logSn_1','logSp_1','logk_1','bandgap_1',"Et_eV_2","Sn_cm2_2","Sp_cm2_2",'k_2','logSn_2','logSp_2','logk_2','bandgap_2']
+}
+PARAM={
+        'type': 'p',                #   Wafer doping type
+        'Et_min_1':Et1_pred,             #   Minimum defect energy level
+        'Et_max_1':Et1_pred,              #   Maximum defect energy level
+        'Et_min_2':0,             #   Minimum defect energy level
+        'Et_max_2':0.55,              #   Maximum defect energy level
+        'S_min_1_p':10**(logSp1_pred),              #   Minimum capture cross section for hole.
+        'S_min_1_n':10**(logSn1_pred),          #   Minimum capture cross section for electron.
+        'S_max_1_p':10**(logSp1_pred),              #   Maximum capture cross section for hole.
+        'S_max_1_n':10**(logSn1_pred),              # maximum capcture cross section for electron.
+        'S_min_2_p':1E-17,              #   Minimum capture cross section for hole.
+        'S_min_2_n':1E-17,          #   Minimum capture cross section for electron.
+        'S_max_2_p':1E-13,              #   Maximum capture cross section for hole.
+        'S_max_2_n':1E-13,              # maximum capcture cross section for electron.
+        'Nt':1E12,                  #   Defect density
+        'check_auger':False,     #   Check wether to resample if lifetime is auger-limited
+        'noise':'',             #   Enable noiseparam
+        'noiseparam':0,         #   Adds noise proportional to the log of Delta n
+}
+# %%-
+
+# %%-- Define experiment: every time remember to run this line to refresh the code.
+exp = Experiment(SaveDir=SAVEDIR, Parameters=PARAMETERS)
+# %%-
+
+# %%-- Data simulation:
+
+# sanity check whether exp is defined.
+print(exp)
+db_sah=DPML.generateDB_sah(PARAMETERS['n_defects'], TEMPERATURE, DOPING, PARAMETERS['dn_range'], PARAM) # one two-level defect data
+# print(db_sah)
+db_sah['Mode']=['Single two-level']*len(db_sah)
+dataDf=db_sah
+dataDf['Label']=[0 if mode=="Two one-level" else 1 for mode in dataDf['Mode']]
+exp.uploadDB(dataDf)
+vocab={
+    '0':'Two one-level',
+    '1':'Single two-level',
+}
+
+# %%-
+
+generated_data = db_sah
+
+# %%-
 
 # %%-- Email reminder
 def email_reminder():
