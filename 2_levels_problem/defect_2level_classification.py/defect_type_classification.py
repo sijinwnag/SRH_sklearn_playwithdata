e066f30f8a5d85d2a1ba815d89c2a0d26a5e2c7d
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-07-13 20:46:00 +1000

    s

--- 2_levels_problem/defect_2level_classification.py/defect_type_classification.py
+++ 2_levels_problem/defect_2level_classification.py/defect_type_classification.py
@@ -1,47 +1,49 @@
-################################################################################
-# import the library
-import numpy as np
+# %%-- Imports
 import pandas as pd
+import numpy as np
 import seaborn as sn
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+from sklearn.metrics import r2_score, mean_absolute_error, confusion_matrix, f1_score, accuracy_score
+from sklearn.linear_model import LinearRegression, Ridge
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.svm import SVR, SVC
 import sys
-
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+# uncomment the below line for dell laptop only
+from playsound import playsound
+from sklearn.model_selection import cross_val_score, RepeatedKFold
+from sklearn.multioutput import RegressorChain
+from semiconductor.recombination import SRH
+import scipy.constants as sc
+from datetime import datetime
+import smtplib
+from email.message import EmailMessage
+import os
+import sys
 # import the function file from another folder:
-sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata')
-from function_for_trainings import classification_repeat, classification_training
-##################################################################################
-# firstly, load the data:
-df = pd.read_csv(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\2level_defects.csv')
+# use this line if on hp laptop:
+# sys.path.append(r'C:\Users\budac\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
+# use this line if on dell laptop
+sys.path.append(r'C:\Users\sijin wang\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
+# use this line if on workstation
+sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2')
+sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\DPML')
+sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Savedir_example')
+from MLobject_tlevel import *
+from dynamic_generation_regression import *
+df1 = MyMLdata_2level(r"G:\study\thesis_data_storage\set11\set11_800k_non_Auger.csv", 'bandgap1',1)
+# df1.data.head()
+# %%-
 
-# identify extract the useful columns
-# delete all the defect information except the defect class.
-delete_col = ['Name', 'Et_eV_1', 'Sn_cm2_1', 'Sp_cm2_1', 'k_1', 'logSn_1', 'logSp_1', 'logk_1', 'bandgap_1', 'Et_eV_2', 'Sn_cm2_2', 'Sp_cm2_2', 'k_2', 'logSn_2', 'logSp_2', 'logk_2', 'bandgap_2']
-dfk = df.drop(delete_col, axis=1)
-
-# encode the column: Mode.
-dfk['Mode'] = pd.Categorical(dfk['Mode'])
-dfk = pd.get_dummies(dfk)
-# dfk.columns.values.tolist()
-dfk = dfk.drop(['Mode_Two one-level'], axis=1)
-# train test split:
-X = dfk.drop(['Mode_Single two-level'], axis=1)
-y = dfk['Mode_Single two-level']
-
-# now, make logX into X and redo the process, compare the f1 scores:
-f1_scoreslog = classification_repeat(X, y, 5)
-avf1scoreslog = np.average(f1_scoreslog, axis=0)
-avf1scoreslog
-models_names = ['KNN', 'SVC', 'Decision tree', 'Random Forest',  'Gradient Boosting', 'Adaptive boosting', 'Naive Bayes', 'Neural Network']
-df_plot = pd.DataFrame({'using original X': avf1scores, 'using logX': avf1scoreslog}, index=models_names)
-ax = df_plot.plot.barh()
-ax.legend(bbox_to_anchor=(1.4, 0.55))
-plt.title('the R2 scores for training using original X vs using logX')
+# %%-- perform classification
+df1.singletask='bandgap_1'
+# try svc by itself
+# X, y = df1.pre_processor()
+f1scores, y_prediction_frame, y_test_frame = df1.classification_repeat()
+# %%-
