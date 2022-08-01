39b8460bb755c95fc9a205cf1aab75f681bb1afc
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-08-02 09:29:32 +1000

    Update set11.py

--- 2_levels_problem/mode2/Et_regression/set11/set11.py
+++ 2_levels_problem/mode2/Et_regression/set11/set11.py
@@ -50,7 +50,7 @@ sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn
 sys.path.append(r'C:\Users\z5183876\OneDrive - UNSW\Documents\GitHub\SRH_sklearn_playwithdata\2_levels_problem\mode2\Savedir_example')
 from MLobject_tlevel import *
 from dynamic_generation_regression import *
-df1 = MyMLdata_2level(r"C:\Users\sijin wang\Desktop\Thesis\thesiswork\simulation_data\Etnonordered\n\set11_80k_n.csv", 'bandgap1',1)
+df1 = MyMLdata_2level(r"C:\Users\sijin wang\Documents\GitHub\yoann_code_new\Savedir_example\outputs\2022-07-29-10-42-19_advanced example - multi_level_L_datasetID_0.csv", 'bandgap1',1)
 # df1.data.head()
 # %%-
 
