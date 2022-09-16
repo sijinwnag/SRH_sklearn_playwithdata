b90a16b2141d3941a69d2125a145b29febcb5e88
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-09-16 13:33:41 +1000

    s

--- 2_levels_problem/mode2/MLobject_tlevel.py
+++ 2_levels_problem/mode2/MLobject_tlevel.py
@@ -109,19 +109,19 @@ class MyMLdata_2level:
         # 'param_list': [{'n_neighbors':range(1, 30)}, {'max_depth': [10, 100, 1e3]}, {'n_estimators':[10, 100]}, {'n_estimators':[10, 100]},{'n_estimators':[10, 100]}, {'var_smoothing':[1e-9, 1e-3]},{'hidden_layer_sizes':((100, 300, 500, 300, 100), (100, 300, 500, 500, 300, 100), (200, 600, 900, 600, 200))}]# a list of key parameters correspond to the models in the model_lists
         # }
         # NN compared to Naive bias only only:
-        # classification_default_param = {
-        # 'model_names': ['Naive Bayes', 'Neural Network'], # a list of name for each model.
-        # 'model_lists': [GaussianNB(), MLPClassifier((100,100),alpha=0.001, activation = 'relu',verbose=0,learning_rate='adaptive')],# a list of model improted from sklearn
-        # 'gridsearchlist': [False, False],
-        # 'param_list': [{'var_smoothing':[1e-9, 1e-3]}, {'hidden_layer_sizes':((100, 300, 500, 300, 100), (100, 300, 500, 500, 300, 100), (200, 600, 900, 600, 200))}]# a list of key parameters correspond to the models in the model_lists
-        # }
-        # wihtout CSV, gradient boosting, adabosting
         classification_default_param = {
-        'model_names': ['KNN', 'Decision tree', 'Random Forest',  'Naive Bayes', 'Neural Network'], # a list of name for each model.
-        'model_lists': [KNeighborsClassifier(n_neighbors = 5, weights='distance',n_jobs=-1), DecisionTreeClassifier(), RandomForestClassifier(n_estimators=100, verbose =0,n_jobs=-1), GaussianNB(), MLPClassifier((100,100),alpha=0.001, activation = 'relu',verbose=0,learning_rate='adaptive')],# a list of model improted from sklearn
-        'gridsearchlist': [False, False, False, False, False, False, False],
-        'param_list': [{'n_neighbors':range(1, 30)}, {'max_depth': [10, 100, 1e3]}, {'n_estimators':[10, 100]}, {'var_smoothing':[1e-9, 1e-3]},{'hidden_layer_sizes':((100, 300, 500, 300, 100), (100, 300, 500, 500, 300, 100), (200, 600, 900, 600, 200))}]# a list of key parameters correspond to the models in the model_lists
+        'model_names': ['Naive Bayes', 'Neural Network'], # a list of name for each model.
+        'model_lists': [GaussianNB(), MLPClassifier((100, 300, 500, 300, 100),alpha=0.001, activation = 'relu',verbose=0,learning_rate='adaptive')],# a list of model improted from sklearn
+        'gridsearchlist': [False, False],
+        'param_list': [{'var_smoothing':[1e-9, 1e-3]}, {'hidden_layer_sizes':((100, 300, 500, 300, 100), (100, 300, 500, 500, 300, 100), (200, 600, 900, 600, 200))}]# a list of key parameters correspond to the models in the model_lists
         }
+        # wihtout CSV, gradient boosting, adabosting
+        # classification_default_param = {
+        # 'model_names': ['KNN', 'Decision tree', 'Random Forest',  'Naive Bayes', 'Neural Network'], # a list of name for each model.
+        # 'model_lists': [KNeighborsClassifier(n_neighbors = 5, weights='distance',n_jobs=-1), DecisionTreeClassifier(), RandomForestClassifier(n_estimators=100, verbose =0,n_jobs=-1), GaussianNB(), MLPClassifier((100,100),alpha=0.001, activation = 'relu',verbose=0,learning_rate='adaptive')],# a list of model improted from sklearn
+        # 'gridsearchlist': [False, False, False, False, False, False, True],
+        # 'param_list': [{'n_neighbors':range(1, 30)}, {'max_depth': [10, 100, 1e3]}, {'n_estimators':[10, 100]}, {'var_smoothing':[1e-9, 1e-3]},{'hidden_layer_sizes':((100, 300, 500, 300, 100), (100, 300, 500, 500, 300, 100), (200, 600, 900, 600, 200), (100, 200))}]# a list of key parameters correspond to the models in the model_lists
+        # }
 
         self.path = path
         self.data = pd.read_csv(path)
@@ -452,7 +452,7 @@ class MyMLdata_2level:
 
 
 # %%--- Classification machine learning tasks.
-    def classification_training(self, X_train_scaled, X_test_scaled, y_train, y_test, display_confusion_matrix=False, return_model=False):
+    def classification_training(self, X_train_scaled, X_test_scaled, y_train, y_test, display_confusion_matrix=True, return_model=False):
         """
         This function is only capable for binary classification yet.
         input:
@@ -505,7 +505,7 @@ class MyMLdata_2level:
             y_pred_list.append(y_pred)
             y_test_list.append(y_test)
             # print the output
-            print('finish training ' + name + ', the accuracy is ' + str(f1))
+            print('finish training ' + name + ', the F1 score is ' + str(f1))
             # display the confusion matrix
             if display_confusion_matrix==True:
                 print(confusion_matrix(y_test, y_pred, normalize='all'))
